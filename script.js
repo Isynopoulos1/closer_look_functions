eb7e05e9da5d979e41e845a1b83d20fdc9f90b9c
Isela Alarcón <isela.v.alarcon@gmail.com>
2023-01-27 15:29:31 +0100

    functions accepting callbacks functions

--- script.js
+++ script.js
@@ -1,22 +1,78 @@
 'use strict';
 
-const bookings = [];
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers // we turned a dynamic price
-) {
-  //ES5 SINTAX
-  // numPassengers = numPassengers || 1;
-  // price = price || 199;
+// const bookings = [];
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers // we turned a dynamic price
+// ) {
+//   //ES5 SINTAX
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+// createBooking('LH234');
+// createBooking('LH234', 3);
+
+// const flight = 'LH536';
+// const isela = {
+//   name: 'Isela Alarcon',
+//   passport: 'G345673833',
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LY673';
+//   passenger.name = 'Mrs' + passenger.name;
+
+//   if (passenger.passport === 'G345673833') {
+//     alert('checked in!');
+//   } else {
+//     alert('wrong password');
+//   }
+// };
+// checkIn(flight, isela);
+// console.log(flight);
+// console.log(isela);
+
+// // is the same as doing...
+// const flightNum = flight;
+// const passenger = isela;
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 100000000000);
+// };
+// newPassport(isela);
+// checkIn(flight, isela);
+
+// FUNCTIONS ACCEPTING CALLBACKS FUNCTIONS
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
 };
-createBooking('LH234');
-createBooking('LH234', 3);
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
+
+const high5 = function () {
+  console.log('👋🏽');
+};
+
+document.body.addEventListener('click', high5);
