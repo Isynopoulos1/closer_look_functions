55901c9cf27cf415f33a5dc8ccf18a9b6de3edfa
Isela Alarcón <isela.v.alarcon@gmail.com>
2023-01-31 14:39:16 +0100

    closure functions

--- script.js
+++ script.js
@@ -52,25 +52,29 @@
 // checkIn(flight, isela);
 
 // FUNCTIONS ACCEPTING CALLBACKS FUNCTIONS
-
+// G IS FOR GLOBAL MODIFY
 const oneWord = function (str) {
   return str.replace(/ /g, '').toLowerCase();
 };
 
+//TO SWITCH THE FIRST WORD TO UPPERCASE
 const upperFirstWord = function (str) {
   const [first, ...others] = str.split(' ');
   return [first.toUpperCase(), ...others].join(' ');
 };
 
-//higher-order function
+console.log(upperFirstWord('hola, hola'));
+
+// /////////////////////// HIGHER ORDER  FUNCTIONS
 const transformer = function (str, fn) {
   console.log(`Original string: ${str}`);
   console.log(`Transformed string: ${fn(str)}`);
   console.log(`Transformed by: ${fn.name}`);
 };
-transformer('Javascript is the best!', upperFirstWord);
+transformer('javascript is the best! 2', upperFirstWord);
 transformer('Javascript is the best!', oneWord);
 
+//JS uses callbacks all the time
 const high5 = function () {
   console.log('👋🏽');
 };
@@ -79,3 +83,36 @@ document.body.addEventListener('click', high5);
 
 //pasa el evento a los 3 elementos del array
 ['isela', 'erwan', 'benito'].forEach(high5);
+
+// const exampleOneWorld = function (str) {
+//   return str.replace(/ /g, '').toUpperCase();
+// };
+
+// const toLowerCaseExample = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toLowerCase(), ...others].join(' ');
+// };
+
+// const transformer2 = function (str, fn) {
+//   console.log(`SOY UN STRING ${str}`);
+//   console.log(`SOY UN STRING ${fn(str)}`);
+// };
+// transformer2('SOY Un EjEmplo', exampleOneWorld);
+// transformer2('SOY Un EjEmplo 2', toLowerCaseExample);
+
+// /////////////////////// CLOSURE FUNCTIONS
+
+const greet = gretting => {
+  return name => {
+    console.log(`${gretting} ${name}`);
+  };
+};
+
+// SOLUTION 1 greet stored in a variable and assigning a name parameter
+// const greetJonas1 = greet('hey');
+// greetJonas1('jonas');
+
+// SOLUTION 2 greet stored in a variable with both parameters on it
+const greetJonas2 = greet('hey')('jonas');
+
+// EXPECTED OUTPUT hey jonas
